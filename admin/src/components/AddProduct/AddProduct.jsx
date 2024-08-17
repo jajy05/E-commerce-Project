84336bb4db45df2e75567e420cdf9b032a21b595
Jajwalya wani <119395513+jajy05@users.noreply.github.com>
2024-08-17 18:48:17 +0530

    Update AddProduct.jsx

--- admin/src/components/AddProduct/AddProduct.jsx
+++ admin/src/components/AddProduct/AddProduct.jsx
@@ -26,7 +26,7 @@ const AddProduct=()=>{
         let product =productDetails;
         let formData= new FormData();
         formData.append('product',image);
-        await fetch('http://localhost:4000/upload',{
+        await fetch('https://jshop-backend.onrender.com/upload',{
             method:'POST',
             headers:{Accept:'application/json',
 
@@ -35,7 +35,7 @@ const AddProduct=()=>{
         }).then((resp)=>resp.json()).then((data)=>{responseData=data});
         if(responseData.success){ product.image=responseData.image_url;
             console.log(product);
-            await fetch('http://localhost:4000/addproduct',{
+            await fetch('https://jshop-backend.onrender.com/addproduct',{
                 method:'POST',
                 headers:{
                     Accept:'application/json',
