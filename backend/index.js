b99086f280f811a164ff4646a54f8e0cad58adef
Jajwalya wani <119395513+jajy05@users.noreply.github.com>
2024-08-17 11:31:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -37,7 +37,7 @@ app.use('/images', express.static('upload/images'));
 app.post("/upload", upload.single('product'), (req, res) => {
     res.json({
         success: 1,
-        image_url: `https://jshop-backend.onrender.com/images/${req.file.filename}`
+        image_url: `https://jshop-backend.onrender.com/upload/images/${req.file.filename}`
     });
 });
 
